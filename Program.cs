b08c72cb882786c189f6f287f041e75ecf74c443
Ana Ohama <apaula-l@student.42sp.org.br>
2024-01-12 10:09:16 -0300

    Sending project

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Nokia nokia = new Nokia("999999999", "Nokia 3310", "123456789", 16);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("TikTok");
+
+        Iphone iphone = new Iphone("999999999", "Iphone 11", "123456789", 64);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
